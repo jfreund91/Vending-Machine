65f0eb292a42b6cac788b26df26e66f71f513cf2
Mariah Dawson <mlynndawson@gmail.com>
2019-02-07 11:11:21 -0500

    first save to vending machine save

---
+++ Capstone/Classes/InventoryReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace Capstone.Classes
+{
+    public class InventoryReader
+    {
+        public static void ReadInventory()
+        {
+            using (StreamReader sr = new StreamReader("vendingmachine.csv"))
+            {
+                while(!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+
+                }
+            }
+        }
+    }
+}
---
+++ Capstone/Classes/MainMenu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Classes;
+using System.IO;
+
+namespace Capstone.Classes
+{
+    public class MainMenu
+    {
+        public void Run()
+
+        {
+            while(true)
+            {
+                Console.WriteLine("WELCOME to Vendo-Matic500");
+                Console.WriteLine("________________________");
+                Console.WriteLine("||      ooOOOOoo      ||");
+                Console.WriteLine("||     0000000000     ||");
+                Console.WriteLine("||     0   00   0     ||");
+                Console.WriteLine("||     0oo0000oo0     ||");
+                Console.WriteLine("||       o0oo0o       ||");
+                Console.WriteLine("||        0000        ||");
+                Console.WriteLine("||____________________||");
+                Console.WriteLine("||____________________||");
+                Console.WriteLine("||  ====     _______  ||");
+                Console.WriteLine("||  |()|     [     ]  ||");
+                Console.WriteLine("||  ====     [_____]  ||");
+                Console.WriteLine("||____________________||");
+                Console.WriteLine("========================");
+
+                Console.WriteLine("Please make a choice.");
+                Console.WriteLine("(1) Display Vending Machine Items");
+                Console.WriteLine("(2) Purchase");
+                Console.WriteLine("(Q) Quit");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    //TODO display menu vending machine class
+                    using (StreamReader sr = new StreamReader(@"\Users\Mariah Dawson\Pairs\c-module-1-capstone-team-4\Capstone\Classes\vendingmachine.csv"))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+                    }
+
+                }
+                if (choice == "2")
+                {
+                    //TODO 
+                }
+                if (choice == "Q" || choice == "q")
+                {
+                    break;
+                }
+               else
+                {
+                    Console.WriteLine("Invalid input.'");
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
