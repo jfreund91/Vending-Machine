5e1dd2ed80d1ad9faa2f1d14646dea302f948676
Mariah Dawson <mlynndawson@gmail.com>
2019-02-08 11:52:32 -0500

    unit test 2

--- CapstoneTests/UnitTest1.cs
+++ CapstoneTests/UnitTest1.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace CapstoneTests
 {
     [TestClass]
-    public class VendingMachineTest
+    public class FeedMoneyTest
     {
         [TestMethod]
         public void FeedMoneyTest_HappyRoad()
@@ -19,5 +19,35 @@ namespace CapstoneTests
             Assert.AreEqual(5, happyRoad.Balance, "Balance should be updated to $5.");
 
         }
+
+        public void FeedMoneyTest_InvalidBill_DoesNotProceed()
+        {
+            InventoryReader inventoryReader = new InventoryReader();
+            Dictionary<string, VendingMachineItem> inventory = inventoryReader.ReadInventory();
+
+            VendingMachineBrain badRoad = new VendingMachineBrain(inventory);
+            badRoad.FeedMoney(8.75M);
+            Assert.AreEqual(0, badRoad.Balance, "Balance should not be updated.");
+
+        }
     }
+
+    [TestClass]
+    public class ChargeTest
+    {
+        [TestMethod]
+        public void ChargeTest_RemovesProperAmount()
+        {
+            InventoryReader inventoryReader = new InventoryReader();
+            Dictionary<string, VendingMachineItem> inventory = inventoryReader.ReadInventory();
+
+            VendingMachineBrain happyRoad = new VendingMachineBrain(inventory);
+            happyRoad.FeedMoney(5);
+            happyRoad.AddToCart("A1");
+            happyRoad.Charge();
+            Assert.AreEqual(1.95M, happyRoad.Balance, "Item added updates the balance");
+        }
+    }
+
+
 }
